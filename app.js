5967a11ec09296d74e264e66974534b640da6b28
adityacaturputra <adityacaturputra25@gmail.com>
2021-12-28 22:48:11 +0700

    enable cors

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 const methodOverride = require('method-override');
 const session = require('express-session');
 const flash = require('connect-flash');
+const cors = require('cors')
 
 // import mongoose
 const mongoose = require('mongoose');
@@ -27,6 +28,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+// cors
+app.use(cors())
 
 // use method-override
 app.use(methodOverride('_method'));
