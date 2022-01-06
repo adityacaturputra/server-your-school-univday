b4737088eabc52c13145fefb597778c8c87a38c2
adityacaturputra <adityacaturputra25@gmail.com>
2022-01-06 22:34:28 +0700

    delete schedule when university related with schedule id

--- controllers/adminController/university/deleteUniversity.js
+++ controllers/adminController/university/deleteUniversity.js
@@ -2,6 +2,7 @@ const Image = require('../../../models/Image');
 const Users = require('../../../models/Users');
 const University = require('../../../models/University');
 const Content = require('../../../models/Content');
+const Schedule = require('../../../models/Schedule');
 
 module.exports = async (req, res) => {
   try {
@@ -11,6 +12,7 @@ module.exports = async (req, res) => {
     await university.remove();
     await image.remove();
     await Content.remove({ universityId: university._id });
+    await Schedule.remove({ universityId: id });
     await Users.remove({ universityId: id });
     req.flash('alertMessage', 'Success delete university');
     req.flash('alertStatus', 'success');
